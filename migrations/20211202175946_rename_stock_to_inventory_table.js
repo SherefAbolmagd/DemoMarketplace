ab93e292955dd95ede0adf1a7bdecd47ef2a894b
Sheref Abolmagd <sherefabolmagd2010@gmail.com>
2023-03-14 17:18:14 +0800

    Migrate

--- migrations/20211202175946_rename_stock_to_inventory_table.js
+++
@@ -1,8 +0,0 @@
-
-exports.up = function(knex) {
-    return knex.schema.renameTable("stocks","inventories");
-};
-
-exports.down = function(knex) {
-    return knex.schema.renameTable("inventories","stocks");
-};
